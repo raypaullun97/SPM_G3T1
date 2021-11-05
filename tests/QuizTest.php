c2d5decaf7971b8b09d0107e3674e1a0bd78344a
Ian Leong <ian.leong.2019@scis.smu.edu.sg>
2021-11-06 02:34:29 +0800

    Update QuizTest.php

--- tests/QuizTest.php
+++ tests/QuizTest.php
@@ -1,13 +1,15 @@
 <?php 
 require './model/Quiz.php';
 require './model/QuizDAO.php';
+$quiz = new QuizDAO();
 
 class QuizTest extends \PHPUnit\Framework\TestCase
 {   
     public function testInsertQuiz()
     {
-        $quiz = new QuizDAO();
         $status = $quiz->createQuiz('G2', 'IS212', '8', '3', '50', '10', 'Ungraded', 'TestTDD');
         $this->assertEquals(true, $status);
     }
+
+
 }
