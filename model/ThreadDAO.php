aab5b5a1503f978e589c44919fe01c7de3b72dd2
Ian Leong <ian.leong.2019@scis.smu.edu.sg>
2021-11-08 22:04:42 +0800

    Update minor errors

--- model/ThreadDAO.php
+++ model/ThreadDAO.php
@@ -2,15 +2,15 @@
     class ThreadDAO{
 
         # Add a new user to the database
-        public function add($forum_id, $description, $engineer_id){
+        public function add($forum_id, $t_description, $engineer_id){
             $conn_manager = new ConnectionManager();
             $pdo = $conn_manager->getConnection();
             
-            $sql = "insert into thread (forum_id, description, engineer_id) 
-                    values (:forum_id, :description, :engineer_id)";
+            $sql = "insert into thread (forum_id, t_description, engineer_id) 
+                    values (:forum_id, :t_description, :engineer_id)";
             $stmt = $pdo->prepare($sql);
             $stmt->bindParam(":forum_id",$forum_id);
-            $stmt->bindParam(":description",$description);
+            $stmt->bindParam(":t_description",$t_description);
             $stmt->bindParam(":engineer_id",$engineer_id);
             $status = $stmt->execute();
             
--- tests/ThreadTest.php
+++ tests/ThreadTest.php
@@ -11,7 +11,7 @@ class ThreadTest extends \PHPUnit\Framework\TestCase
 
         $dsn = "mysql:host=127.0.0.1;dbname=lms;port=3306";
         $pdo = new PDO($dsn,"root",'');
-        $sql = 'delete from thread where forum_id = "100" and description = "Testing TDD" and engineer_id = "1"';
+        $sql = 'delete from thread where forum_id = "100" and t_description = "Testing TDD" and engineer_id = "1"';
         $stmt = $pdo->prepare($sql);
 
         $stmt->execute();
